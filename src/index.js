bd2998dd0abf5d5e15b7881f07c831d25c4e7fd3
Shivam <shivam.si1404@gmail.com>
2026-09-20 23:01:30 +0530

    Utils Setup and Error handling

--- src/index.js
+++ src/index.js
@@ -5,4 +5,19 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+
+
+connectDB()
+.then(()=>{
+    const server = app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+    });
+    server.on("error",(error)=>{
+        console.log("Error",error);
+        
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed");
+})
+
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,11 @@
+const asyn_handler = (fn) => async(req,res,next) => {
+    try{
+        await fn(req,res,next)
+    }catch(error)
+    {
+        res.status(error.code||500).json({
+            success: false,
+            massage: err.massage
+        })
+    }
+}
